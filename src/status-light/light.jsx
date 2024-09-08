497aa89195e032a80b51e593ff3d313d8abdff06
Cory Sauder <corys@getkinectiv.com>
2024-09-08 16:08:41 +0200

    light

--- src/status-light/light.jsx
+++ src/status-light/light.jsx
@@ -5,7 +5,7 @@ function Light(){
 
     function updateLight(){
         const xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://homeassistant.local:8123/api/services/light/turn_on");
+        xhr.open("POST", "homeassistant.local:8123/api/services/light/turn_on");
         xhr.setRequestHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0MzliNjBjMzNlYTc0MTkwYTRjM2QwNjI5Y2Y5M2VmOSIsImlhdCI6MTcyNTc5Njk0MiwiZXhwIjoyMDQxMTU2OTQyfQ.Vbw0E7LMsAfbsr_l-vSwMNUR72Mb_JOPtcrZ6V47Z-U")
       
         const body = JSON.stringify({
